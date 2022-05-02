13b7f4629e544fd9c447d2fc6cbc23ee25a4c966
3mstecnologia <matheus@>
2022-05-02 17:23:12 -0300

    widget animado ok

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-  return (
-    <button
-      className="
-    bg-slate-500
-    p-2
-    rounded-sm	"
-    >
-      {props.text}
-    </button>
-  );
+export function App() {
+  return <Widget />;
 }
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="Hello" />
-      <Button text="01" />
-      <Button text="qqq" />
-      <Button text="aaaa" />
-    </div>
-  );
-}
-
-export default App;
