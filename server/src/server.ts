5f84679a4ef66a70cd85f489b8243a9ec7bd7d0a
3mstecnologia <matheus@>
2022-05-04 20:31:32 -0300

    salvando e enviando email antes do SOLID

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "cde5827594f25b",
+    pass: "bc9bd738cadbb4",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   console.log(req.body);
@@ -12,7 +22,17 @@ app.post("/feedbacks", async (req, res) => {
       ...req.body,
     },
   });
-  return res.status(201);
+  await transport.sendMail({
+    from: "matheus8888@gmail.com",
+    to: "matheus@uniplac.net",
+    subject: "Novo Feedback",
+    html: [
+      `<p> ${req.body.type}</p>`,
+      `<p> ${req.body.comment}</p>`,
+      `<p> ${req.body.screenshot}</p>`,
+    ].join("\n"),
+  });
+  return res.status(201).send({ message: "Feedback enviado com sucesso" });
 });
 
 app.listen(3333, () => {
